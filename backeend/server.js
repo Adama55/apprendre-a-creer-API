138c5e1a07f5dd79455c09303617f32237276f95
Adama55 <mafahfof55@gmail.com>
2022-10-05 23:52:02 +0200

    modification

--- backeend/server.js
+++ backeend/server.js
@@ -2,13 +2,13 @@
  emporter le package http
 */
 const http = require('http');
+const app = require('./app');
 
 /*
-on cree une requette avec deux argument avec req et res
+on cree une requette avec deux argument 
  */
-const server = http.createServer((req, res) => {
-    res.end('voila la reponse du serveur !');
-});
+app.set('port', process.env.PORT || 3000); // dire a app sur quel elle va tourner
+const server = http.createServer(app);
 
 /*
 on va ecouter le port et par defaut le 3000
