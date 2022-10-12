494a899f4ead32394b42166b1442e4386b3fb143
Adama55 <mafahfof55@gmail.com>
2022-10-12 23:54:22 +0200

    ajou app

--- backeend/app.js
+++ backeend/app.js
@@ -1,11 +1,60 @@
 const express = require('express');
+const bodyPaser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.use((req, res) => {
-    res.json({
-        massage : ' voila requete fonctionne bien'
+mongoose.connect('mongodb+srv://Adama55:Fofana232@atlascluster.q4jzdlf.mongodb.net/test',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// permetre aux utilisateur de creer leur objet
+/*
+Avec ceci, Express prend toutes les requêtes qui 
+ont comme Content-Type  application/json  
+et met à disposition leur  body  directement sur l'objet req
+*/
+app.use(express.json());
+//
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  // exemeple post
+  app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+      message: 'Objet créé !'
     });
-});
+  });
+
+
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+  });
 
 module.exports = app;
